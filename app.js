d37c0fe75b2e45eb1299f35ffefce825b772e06f
aplui <36481473+aplui@users.noreply.github.com>
2020-11-16 20:34:20 +0100

    add mongodb link

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-//vereiste packages
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express();
 const mongoose = require('mongoose');
 
-//configuratie van packages
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
@@ -12,74 +10,67 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"))
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
-      useNewUrlParser : true});
+  useNewUrlParser: true
+});
 
-      const itemsSchema = {
-        name: String
+const itemsSchema = {name: String}
+const Item = mongoose.model("Item", itemsSchema)
+const item1 = new Item({ name: "Welcome to your ToDoList!"});
+const item2 = new Item({ name: "Hit the + button to add a new item."});
+const item3 = new Item({ name: "<- Hit this to delete an item."});
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function(req, res) {
+
+  Item.find({}, function(err, foundItems) {
+  if (foundItems.length === 0) {
+    Item.insertMany(defaultItems, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Succesfully saved default items to DB!");
       }
+    });
+    res.redirect("/");
+  } else {
+    res.render("list", {
+      listTitle: "Today",
+      newListItems: foundItems
+    });
 
-      const Item = mongoose.model("Item", itemsSchema)
+  }
 
-      const item1 = new Item({
-        name: "Welcome to your ToDoList!"
-      });
+  })
 
-      const item2 = new Item({
-        name: "Hit the + button to add a new item."
-      });
+  });
 
-      const item3 = new Item({
-        name: "<- Hit this to delete an item."
-      });
+app.get("/work", function(req, res) {
 
-      const defaultItems = [item1, item2, item3];
+  res.render("list", {
+    listTitle: "Today",
+    newListItems: workItems
+  });
+});
 
-      Item.insertMany(defaultItems, function(err){
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Succesfully saved default items to DB!");
-        }
-      })
-      //de gets sturen door naar bepaalde directories binnen de website
-      app.get("/", function(req, res) {
+app.get("/about", function(req, res) {
 
-        //toLocaleDateString om de huidige dag te krijgen, options bepaalt hoe de text wordt uitgeschreven.
-        res.render("list", {
-          // kindOfDay verbindt met 2 variables in list.ejs
-          listTitle: day,
-          newListItems: items
-        });
-      });
+  res.render("about")
+})
 
-      app.get("/work", function(req, res) {
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
 
-        res.render("list", {
-          listTitle: "Today",
-          newListItems: workItems
-        });
-      });
-      app.get("/about", function(req, res) {
-
-        res.render("about")
-      })
-
-
-      //post in de home route voor het object met de naam newItem ( de input dus )
-      app.post("/", function(req, res) {
-        let item = req.body.newItem;
-
-        if (req.body.list === "Work") {
-          workItems.push(item);
-          res.redirect("/work")
-        } else {
-          items.push(item)
-          res.redirect("/")
-        }
-      })
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work")
+  } else {
+    items.push(item)
+    res.redirect("/")
+  }
+})
 
 
 
-      app.listen(3000, function() {
-        console.log("Server is running on port 3000");
-      });
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
